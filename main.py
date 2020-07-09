3ed8553c6c36965bb15fff7567dfef1a62f7248b
AndresBerrioSeti <48457170+AndresBerrioSeti@users.noreply.github.com>
2020-07-09 15:50:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hola():
-    return '¡Hola, alumnos de platzi!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
