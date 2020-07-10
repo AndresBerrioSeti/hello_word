51c8ad76dc315a52fd8c5200f74e65c3dd38d91e
AndresBerrioSeti <48457170+AndresBerrioSeti@users.noreply.github.com>
2020-07-10 16:19:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hola():
-    return render_template('index.html')
+@app.route(r'/', methods=['GET'])
+def contact_book():
+    return render_template('contact_book.html')
 
+
+@app.route(r'/add')
+def add_contact():
+    return render_template('add_contact.html')
 
 if __name__ == '__main__':
     app.run()
