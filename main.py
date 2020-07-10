46b53d4ec4e297b811f7342401e1d5c222bc4704
AndresBerrioSeti <48457170+AndresBerrioSeti@users.noreply.github.com>
2020-07-10 17:00:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from contact_model import Contact
 
 app = Flask(__name__)
 
@@ -8,8 +9,14 @@ def contact_book():
     return render_template('contact_book.html')
 
 
-@app.route(r'/add')
+@app.route(r'/add', methods=['GET', 'POST'])
 def add_contact():
+
+    if request.from:
+        contact = =Contact(name=request.from.get('name'), request.from.get('phone'), request.from.get('email'))
+        
+        contact.put()
+
     return render_template('add_contact.html')
 
 if __name__ == '__main__':
