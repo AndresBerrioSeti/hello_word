a6e6dfebdcdaf2e053e2cc26a744b551e471928b
AndresBerrioSeti <48457170+AndresBerrioSeti@users.noreply.github.com>
2020-07-12 13:06:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from flask import Flask, render_template, request
+from google.cloud import ndb
 from contact_model import Contact
 
 app = Flask(__name__)
+client = ndb.Client()
 
 
 @app.route(r'/', methods=['GET'])
 def contact_book():
-    return render_template('contact_book.html')
+    return render_template('contact_book.html', contacts=contacts)
 
 
 @app.route(r'/add', methods=['GET', 'POST'])
 def add_contact():
 
     if request.from:
-        contact = =Contact(name=request.from.get('name'), request.from.get('phone'), request.from.get('email'))
-        
-        contact.put()
+        with client.context():
+            contact = Contact(name=request.form.get('name'), request.form.get('phone'), request.form.get('email'))
+            contact.put()
 
     return render_template('add_contact.html')
 
